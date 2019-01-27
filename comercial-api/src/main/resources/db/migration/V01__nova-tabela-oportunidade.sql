8c08f62e98090c5940a937c94ddc95583ad672a2
Ramon <ramonrune@gmail.com>
2019-01-27 18:00:08 -0200

    Update

---
+++ comercial-api/src/main/resources/db/migration/V01__nova-tabela-oportunidade.sql
@@ -0,0 +1,7 @@
+create table oportunidade(
+	id bigint auto_increment not null,
+	nome_prospecto VARCHAR(80) not null,
+	descricao VARCHAR(200) not null,
+	valor decimal(10,2),
+	primary key(id)
+);
